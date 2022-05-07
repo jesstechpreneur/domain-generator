236baaf49537f087445b8b17a1753807d3731c6c
Jess <hi@jesstechpreneur.com>
2022-05-07 00:20:12 +0000

    Project Files

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#generate").innerHTML = domainName();
+};
+let domainName = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".net", ".biz"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domain.length; l++) {
+          return (domainName = pronoun[i] + adj[j] + noun[k] + domain[l]);
+        }
+      }
+    }
+  }
 };
